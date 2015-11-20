9fe4d66d616864cb9dd50df62a7aa107a260c44a
unknown <sh010407@nate.com>
2015-11-20 13:26:35 +0900

    에러고침

--- app/src/main/java/org/androidtown/diaryapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/diaryapplication/MainActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.androidtown.diaryapplication.db.ScheduleDatabase;
 
-import java.text.SimpleDateFormat;
+import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -21,27 +23,43 @@ public class MainActivity extends Activity {
 
     public static final String TAG = "MainActivity";
 
+    ListView mScheduleListView;
+    ScheduleListAdapter mScheduleListAdapter;
+
+    public static ScheduleDatabase mDatabase = null;
+
     Calendar mCalendar = Calendar.getInstance();
     TextView dateBtn;
-    public static SimpleDateFormat dateDayNameFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
 
-    ListView mschedulelistView;
-    ScheduleListAdapter mscheduleListAdapter;
 
     /**
      * 데이터베이스 인스턴스
      */
-    public static ScheduleDatabase mDatabase = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setCalendar();
-        mschedulelistView = (ListView)findViewById(R.id.scheduleList);
-        mscheduleListAdapter = new ScheduleListAdapter(this);
-        mschedulelistView.setAdapter(mscheduleListAdapter);
-        mschedulelistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(this,"NO SDCARD", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            String externalPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            if (!BasicInfo.ExternalChecked && externalPath != null) {
+                BasicInfo.ExternalPath = externalPath + File.separator;
+                Log.d(TAG, "ExternalPath : " + BasicInfo.ExternalPath);
+
+                BasicInfo.DATABASE_NAME = BasicInfo.ExternalPath + BasicInfo.DATABASE_NAME;
+
+                BasicInfo.ExternalChecked = true;
+            }
+        }
+
+        mScheduleListView = (ListView)findViewById(R.id.scheduleList);
+        mScheduleListAdapter = new ScheduleListAdapter(this);
+        mScheduleListView.setAdapter(mScheduleListAdapter);
+        mScheduleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
                 viewMemo(position);
             }
@@ -56,6 +74,8 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent, BasicInfo.REQ_INSERT_ACTIVITY);
             }
         });
+
+        setCalendar();
     }
 
     protected void onStart() {
@@ -64,7 +84,7 @@ public class MainActivity extends Activity {
         openDatabase();
 
         // 메모 데이터 로딩
-        loadMemoListData();
+        loadScheduleListData();
 
 
         super.onStart();
@@ -75,68 +95,73 @@ public class MainActivity extends Activity {
      */
 
     public void openDatabase() {
+
         // open database
         if (mDatabase != null) {
             mDatabase.close();
             mDatabase = null;
         }
 
+
         mDatabase = ScheduleDatabase.getInstance(this);
         boolean isOpen = mDatabase.open();
         if (isOpen) {
-            Log.d(TAG, "Memo database is open.");
+            Log.d(TAG, "Schedule database is open.");
         } else {
-            Log.d(TAG, "Memo database is not open.");
+            Log.d(TAG, "Schedule database is not open.");
         }
     }
     /**
      * 메모 리스트 데이터 로딩
      */
-    public int loadMemoListData() {
 
-        String SQL = "select _id, INPUT_DATE, INPUT_TIME, CONTENT_TEXT, from SCHEDULE ";
+    public int loadScheduleListData() {
+
+        String SQL = "select _id, INPUT_TIME, CONTENT_TEXT  from SCHEDULE order by INPUT_TIME desc ";
 
         int recordCount = -1;
+
         if (MainActivity.mDatabase != null) {
             Cursor outCursor = MainActivity.mDatabase.rawQuery(SQL);
-
-            recordCount = outCursor.getCount();
+             recordCount = outCursor.getCount();
             Log.d(TAG, "cursor count : " + recordCount + "\n");
 
-            mscheduleListAdapter.clear();
+            mScheduleListAdapter.clear();
 
             for (int i = 0; i < recordCount; i++) {
                 outCursor.moveToNext();
 
                 String scheduleId = outCursor.getString(0);
 
-                String dateStr = outCursor.getString(1);
-                if (dateStr != null && dateStr.length() > 10) {
-                    //dateStr = dateStr.substring(0, 10);
+                String dateStr = null;
+
+                String timeStr = outCursor.getString(1);
+                if (timeStr != null && timeStr.length() > 10) {
+
                     try {
-                        Date inDate = BasicInfo.dateFormat.parse(dateStr);
+                        Date inTime = BasicInfo.dateTimeFormat.parse(timeStr);
 
                         if (BasicInfo.language.equals("ko")) {
-                            dateStr = BasicInfo.dateNameFormat2.format(inDate);
+                            timeStr = BasicInfo.dateTimeFormat.format(inTime);
                         } else {
-                            dateStr = BasicInfo.dateNameFormat3.format(inDate);
+                            timeStr = BasicInfo.dateTimeFormat.format(inTime);
                         }
                     } catch(Exception ex) {
                         ex.printStackTrace();
                     }
                 } else {
-                    dateStr = "";
+                    timeStr = "";
                 }
 
                 String scheduleStr = outCursor.getString(2);
 
 
-                mscheduleListAdapter.addItem(new ScheduleListItem(scheduleId, dateStr, scheduleStr));
+                mScheduleListAdapter.addItem(new ScheduleListItem(scheduleId, dateStr, timeStr, scheduleStr));
             }
 
             outCursor.close();
 
-            mscheduleListAdapter.notifyDataSetChanged();
+            mScheduleListAdapter.notifyDataSetChanged();
 
         }
 
@@ -146,16 +171,15 @@ public class MainActivity extends Activity {
 
 
     private void viewMemo(int position) {
+        ScheduleListItem item = (ScheduleListItem)mScheduleListAdapter.getItem(position);
 
-        /*schedulelistitem item = (schedulelistitem)mscheduleListAdapter.getItem(position);
-
-        Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+        Intent intent = new Intent(getApplicationContext(), InsertScheduleActivity.class);
         intent.putExtra(BasicInfo.KEY_MEMO_MODE, BasicInfo.MODE_VIEW);
         intent.putExtra(BasicInfo.KEY_MEMO_ID, item.getId());
-        intent.putExtra(BasicInfo.KEY_MEMO_DATE, item.getData(0));
-        intent.putExtra(BasicInfo.KEY_MEMO_TEXT, item.getData(1));
+        intent.putExtra(BasicInfo.KEY_MEMO_TIME, item.getData(1));
+        intent.putExtra(BasicInfo.KEY_MEMO_TEXT, item.getData(2));
 
-        startActivityForResult(intent, BasicInfo.REQ_VIEW_ACTIVITY);*/
+        startActivityForResult(intent, BasicInfo.REQ_VIEW_ACTIVITY);
     }
 
 
@@ -168,7 +192,7 @@ public class MainActivity extends Activity {
         Calendar calendar = Calendar.getInstance();
         Date date = new Date();
         try {
-            date = dateDayNameFormat.parse(mDateStr);
+            date = BasicInfo.dateDayNameFormat.parse(mDateStr);
         } catch (Exception ex) {
             // Log.d(TAG, "Exception in parsing date : " + date);
         }
@@ -195,13 +219,13 @@ public class MainActivity extends Activity {
         switch(requestCode) {
             case BasicInfo.REQ_INSERT_ACTIVITY:
                 if(resultCode == RESULT_OK) {
-                    loadMemoListData();
+                    loadScheduleListData();
                 }
 
                 break;
 
             case BasicInfo.REQ_VIEW_ACTIVITY:
-                loadMemoListData();
+                loadScheduleListData();
 
                 break;
 
